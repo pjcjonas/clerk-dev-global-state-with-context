1c56d5e2452738d1e0b28bbcd45681a5274ad1fe
pjcjonas <pjcjonas@gmail.com>
2022-12-29 20:16:37 +0200

    removed react content implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,23 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import React, { useContext, useEffect, useState } from "react";
-import { UserContext } from "./app-context/user-context";
 import { Container } from "@mui/material";
 import { SignInSignOut } from "./components/SignInSignOut";
 import { AdminPage } from "./components/AdminPage";
 import { WelcomePage } from "./components/WelcomePage";
 
-
-
 const App: React.FunctionComponent = (): JSX.Element => {
-  const { user, clerk, updateState } = useContext(UserContext);
-  const [signedIn, setSignedIn] = useState(false);
-  const [clerkLoaded, setClerkLoaded] = useState(false);
-
-  useEffect(() => {
-    initialise();
-  }, [user]);
-
-  const initialise = async (): Promise<void> => {
-    await loadClerk();
-    await checkUser();
-  };
-
-  const loadClerk = async (): Promise<void> => {
-    if (clerk) {
-      await clerk.load();
-      setClerkLoaded(clerk.isReady());
-    }
-  };
-
-  const checkUser = async (): Promise<void> => {
-    if (clerk.user && clerk.user !== null) {
-      console.log(clerk.user);
-      setSignedIn(true);
-      // @ts-ignore
-      updateState({ user: clerk.user });
-    }
-  };
-
-  const signOut = async (): Promise<void> => {
-    await clerk
-      .signOut(handleOnSignout, { sessionId: clerk.session?.id })
-      .catch((reason: any) => {
-        handleOnSignoutError(reason);
-      });
-  };
-
-  const handleOnSignout = async (): Promise<void> => {
-    setSignedIn(false);
-    // @ts-ignore
-    updateState({ user: null });
-  };
-
-  const handleOnSignoutError = async (reason: any): Promise<void> => {
-    console.error(reason);
-  };
-
-  const openSignIn = async (): Promise<void> => {
-    clerk.openSignIn();
-  };
-
   return (
     <Container maxWidth="sm">
       <BrowserRouter>
-        {clerkLoaded ? (
+        <React.Fragment>
           <React.Fragment>
-            <React.Fragment>
-              <SignInSignOut
-                signOut={signOut}
-                signedIn={signedIn}
-                openSignIn={openSignIn}
-              />
-              <Routes>
-                <Route path="/" element={<WelcomePage />} />
-                <Route
-                  path="/admin"
-                  element={
-                    signedIn ? <AdminPage /> : <Navigate replace to="/" />
-                  }
-                />
-              </Routes>
-            </React.Fragment>
+            <SignInSignOut />
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+              <Route path="/admin" element={<AdminPage />} />
+            </Routes>
           </React.Fragment>
-        ) : (
-          "Loading..."
-        )}
+        </React.Fragment>
       </BrowserRouter>
     </Container>
   );
--- src/components/LoginStatus/index.tsx
+++ src/components/LoginStatus/index.tsx
@@ -1,14 +1,12 @@
-import React, { useContext } from "react";
-import { UserContext } from "../../app-context/user-context";
+import React from "react";
 import { Typography } from "@mui/material";
 
 export const LoginStatus: React.FunctionComponent = (): JSX.Element => {
-  const { user } = useContext(UserContext);
   return (
     <React.Fragment>
       <Typography variant="body1">
         Login status:{" "}
-        {user && user.id ? `Signed in as ${user.firstName}` : `signed out`}
+        Signed in as __TODO__
       </Typography>
     </React.Fragment>
   );
